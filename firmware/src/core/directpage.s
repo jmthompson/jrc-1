2752b6de3511449fb6cfe2e3f6b7ab17b59e2ad2
Joshua M. Thompson <joshua.thompson@gmail.com>
2023-01-29 00:31:54 -0500

    Move the xmodem direct page locations into the monitor's directpage.s file.

--- firmware/src/core/directpage.s
+++ firmware/src/core/directpage.s
@@ -1,35 +1,12 @@
 ; * (C) 2021 Joshua M. Thompson *
 ; *******************************
-
-;;
-; This file defines the direct page locations for the BIOS and jrcOS
 ;
-; Currently this file has a lot of locations that are only used by one
-; specific routine and could be consolidated to save space.
-;;
+; This file defines global direct page locations for jrcOS
 
-        .exportzp   ptr
-        .exportzp   tmp
-
-        .exportzp   lastblk
-        .exportzp   blkno
-        .exportzp   errcnt
-        .exportzp   crc
-        .exportzp   xmptr
-        .exportzp   xmeofp
-        .exportzp   retry
+        .exportzp   ptr, tmp
 
         .segment "ZEROPAGE"
 
 ; Generic temp locations, used lots of places. VERY temporary!
 ptr:            .res    4
 tmp:            .res    2
-
-; Xmodem routines variables
-lastblk:        .res    1   ; flag for last block
-blkno:          .res    1   ; block number 
-errcnt:         .res    1   ; error counter 10 is the limit
-crc:            .res    2   ; CRC
-xmptr:          .res    3   ; data pointer (two byte variable)
-xmeofp:         .res    3   ; end of file address pointer (2 bytes)
-retry:          .res    2   ; retry counter 
--- firmware/src/monitor/directpage.s
+++ firmware/src/monitor/directpage.s
@@ -3,9 +3,18 @@
 ;
 
         .exportzp   cmd, end_loc, ibuffp, row_end, start_loc
+        .exportzp   blkno, crc, errcnt, lastblk, retry, xmeofp, xmptr
 
         .segment    "ZEROPAGE"
 
+; Xmodem routines variables
+lastblk:    .res    1   ; flag for last block
+blkno:      .res    1   ; block number 
+errcnt:     .res    1   ; error counter 10 is the limit
+crc:        .res    2   ; CRC
+xmptr:      .res    3   ; data pointer (two byte variable)
+xmeofp:     .res    3   ; end of file address pointer (2 bytes)
+retry:      .res    2   ; retry counter 
 ibuffp:     .res    4
 cmd:        .res    1
 start_loc:  .res    3
