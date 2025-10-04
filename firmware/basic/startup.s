a78a047c840cb1425bfa2cab0c36905ab3e76414
Joshua M. Thompson <jmt@area73.org>
2025-10-03 21:50:32 -0400

    New directory structure and build system. The new system allows for the addition of arbitrary rom apps in the final firmware image. Each rom app is built separately with its own config, and then the results are combined into the final rom.bin.

---
+++ firmware/basic/startup.s
@@ -0,0 +1,13 @@
+; *******************************
+; *          jrcBASIC           * 
+; * (C) 2025 Joshua M. Thompson *
+; *******************************
+
+        .include "common.inc"
+
+        .segment "CODE"
+
+; placeholder
+
+startup:
+        rtl
